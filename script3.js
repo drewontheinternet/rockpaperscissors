7ce1f763c782462f1eae4277819332aefb05af2a
drewontheinternet <portalatin.drew@gmail.com>
2026-02-19 13:12:03 -0500

    Adding UI to RPS
    added buttons, let the function take the text content of the button, and added a score panel.

--- script3.js
+++ script3.js
@@ -1,78 +1,90 @@
-
-
-function getComputerChoice () {
+function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
         return "rock"
     }
-    else if (randomNumber === 1){
+    else if (randomNumber === 1) {
         return "paper"
     }
     else {
         return "scissors"
     }
+
+}
+
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+const score = document.querySelector(".score")
+
+function playGame(humanChoice) {
+
+
+    const computerChoice = getComputerChoice();
+
+    if (humanChoice === computerChoice) {
+        console.log("Its a tie!")
+    }
+    else if
+        (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore++;
+        console.log("You win! Rock beats scissors.");
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore++;
+        console.log("You win! Paper beats Rock");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        console.log("You win! Scissors beats Paper.");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        computerScore++;
+        console.log("You lose! Rock beats Scissors.");
+    }
+    else if (humanChoice === "rock" && computerChoice === "paper") {
+        computerScore++;
+        console.log("You lose! Paper beats Rock.");
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        computerScore++;
+        console.log("You lose! Scissors beats Paper.");
+    }
+
+
     
-}
-
-function getHumanChoice () {
-    let userInput = prompt("Rock, Paper, or Scissors?").toLowerCase().trim();
-    return userInput
-}
-
-
-
-
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (let round = 1; round <= 5; round++) {
-        console.log("Round: " + round);
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-
-        if (humanChoice === computerChoice) {
-            console.log("Its a tie!")
-        }
-        else if
-            (humanChoice === "rock" && computerChoice === "scissors") {
-            humanScore++;
-            console.log("You win! Rock beats scissors.");
-        }
-        else if (humanChoice === "paper" && computerChoice === "rock") {
-            humanScore++;
-            console.log("You win! Paper beats Rock");
-        }
-        else if (humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-            console.log("You win! Scissors beats Paper.");
-        }
-        else if (humanChoice === "scissors" && computerChoice === "rock") {
-            computerScore++;
-            console.log("You lose! Rock beats Scissors.");
-        }
-        else if (humanChoice === "rock" && computerChoice === "paper") {
-            computerScore++;
-            console.log("You lose! Paper beats Rock.");
-        }
-        else if (humanChoice === "paper" && computerChoice === "scissors") {
-            computerScore++;
-            console.log("You lose! Scissors beats Paper.");
-        }
+    score.textContent = `Human Score: ${humanScore} | Computer Score: ${computerScore}`;
+    if (humanScore === 5) {
+        score.textContent = "You won the game!"
+        resetGame();
+        return;
     }
-    if (humanScore > computerScore) {
-        console.log("You win!")
-    }
-    else if (humanScore < computerScore) {
-        console.log("You lose!")
-    }
-    else {
-        console.log("It's a tie!")
+    else if (computerScore === 5) {
+       score.textContent = "You lost the game!"
+       resetGame();
+       return;
     }
 }
 
+function resetGame () {
+    humanScore = 0
+    computerScore = 0;
+}
 
-playGame ();
+let buttons = document.querySelectorAll(".choice")
+buttons.forEach(button => {
+    button.addEventListener("click", function (event) {
+        const humanChoice = event.currentTarget.textContent;
+        playGame(humanChoice);
+    })
+})
+
+
+
+
+
 
 
 
